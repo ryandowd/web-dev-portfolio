e482aa0b8c1c290cf6d81c964ebe6714afa56834
ryandowd <ryan.dowd84@gmail.com>
2023-03-08 19:01:26 +0800

    Remove getServerSession to debug issue on Prod

--- src/pages/auth/signin.tsx
+++ src/pages/auth/signin.tsx
@@ -33,14 +33,14 @@ export default function SignIn({
 }
 
 export async function getServerSideProps(context: GetServerSidePropsContext) {
-  const session = await getServerSession(context.req, context.res, authOptions);
+  // const session = await getServerSession(context.req, context.res, authOptions);
 
-  // If the user is already logged in, redirect.
-  // Note: Make sure not to redirect to the same page
-  // To avoid an infinite loop!
-  if (session) {
-    return { redirect: { destination: '/' } };
-  }
+  // // If the user is already logged in, redirect.
+  // // Note: Make sure not to redirect to the same page
+  // // To avoid an infinite loop!
+  // if (session) {
+  //   return { redirect: { destination: '/' } };
+  // }
 
   const providers = await getProviders();
 

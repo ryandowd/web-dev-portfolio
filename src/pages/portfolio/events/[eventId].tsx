10c64ecb0ad9339a22d6a7157f062bdf2e2fa69b
ryandowd <ryan.dowd84@gmail.com>
2023-03-16 17:07:55 +0800

    Debugging issue on prod

--- src/pages/portfolio/events/[eventId].tsx
+++ src/pages/portfolio/events/[eventId].tsx
@@ -12,9 +12,16 @@ export default function EventPage(props: EventPageProps) {
 
 export async function getServerSideProps(context: any) {
   const { params } = context;
+
+  console.log('params', JSON.stringify(params, null, 2));
   const eventId = params.eventId;
   const client = await connectToDatabase();
+
+  console.log('client', JSON.stringify(client, null, 2));
+
   const event = await getEvent(eventId, client);
+
+  console.log('event', JSON.stringify(event, null, 2));
 
   delete event._id;
 

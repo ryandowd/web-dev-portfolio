20bed3439719c1876d2f44a760ed1d6e1098f3a2
ryandowd <ryan.dowd84@gmail.com>
2023-03-16 16:39:53 +0800

    Fixing Vercel deploy bug due to letter-casing change in file path

---
+++ src/components/ui/GlobalNav.tsx
@@ -0,0 +1,24 @@
+import { Container } from '@mui/material';
+
+interface GlobalNavProps {
+  children: React.ReactNode;
+}
+
+export const GlobalNav = (props: GlobalNavProps) => {
+  const { children } = props;
+  return (
+    <Container
+      component='nav'
+      maxWidth={false}
+      sx={{ p: 3, backgroundColor: '#000' }}
+    >
+      <Container
+        component='main'
+        maxWidth='xs'
+        sx={{ display: 'flex', justifyContent: 'center' }}
+      >
+        {children}
+      </Container>
+    </Container>
+  );
+};

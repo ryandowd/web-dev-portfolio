6df0c960055af3b0cc960de50e837d60b7e08c4a
ryandowd <ryan.dowd84@gmail.com>
2023-04-16 15:44:51 +0800

    Debug finance page

--- src/sites/finance/components/SnapshotDetailOverview.tsx
+++ src/sites/finance/components/SnapshotDetailOverview.tsx
@@ -37,7 +37,124 @@ export const SnapshotDetailOverview = (props: SnapshotDetailOverviewProps) => {
         borderRadius: 0,
       }}
     >
-      Hello
+      <Box
+        key={snapshotId}
+        sx={{
+          padding: '20px',
+          display: {
+            sm: 'flex',
+          },
+          width: '100%',
+          flexDirection: {
+            xs: 'column',
+            md: 'row',
+          },
+        }}
+      >
+        <Box
+          sx={{
+            display: 'flex',
+            flex: '0 0 20%',
+            textAlign: {
+              xs: 'center',
+              sm: 'left',
+            },
+            flexDirection: 'column',
+            marginBottom: {
+              xs: '20px',
+              lg: 0,
+            },
+            alignItems: {
+              xs: 'center',
+              lg: 'flex-start',
+            },
+          }}
+        >
+          <Box
+            sx={{
+              marginBottom: {
+                xs: '10px',
+                md: '30px',
+              },
+              display: 'flex',
+            }}
+          >
+            <Typography
+              variant='h5'
+              sx={{
+                fontSize: {
+                  xs: '3rem',
+                  md: '2.5rem',
+                },
+                padding: '5px 5px 5px 0',
+                color: '#b7b7b7',
+              }}
+            >
+              {dayjs(snapshotDate).format('DD')}
+            </Typography>
+            <Typography
+              variant='h5'
+              sx={{
+                fontSize: {
+                  xs: '3rem',
+                  md: '2.5rem',
+                },
+                padding: '5px 5px 5px 0',
+                color: '#424242',
+              }}
+            >
+              {dayjs(snapshotDate).format('MMM')}
+            </Typography>
+            <Typography
+              variant='h5'
+              sx={{
+                fontSize: {
+                  xs: '3rem',
+                  md: '2.5rem',
+                },
+                padding: '5px',
+                color: '#7f7f7f',
+              }}
+            >
+              {dayjs(snapshotDate).format('YYYY')}
+            </Typography>
+          </Box>
+          {total && (
+            <Typography variant='h5' sx={{ fontSize: '2.2rem' }}>
+              £{formatNumberWithCommas(total)}
+            </Typography>
+          )}
+          {monthDifference ? (
+            <Box
+              sx={{
+                display: 'flex',
+                flexDirection: 'row',
+                alignItems: 'center',
+                marginTop: {
+                  xs: '20px',
+                  md: '5px',
+                },
+              }}
+            >
+              <Typography
+                variant='h5'
+                sx={{
+                  color: differenceColour,
+                  display: 'flex',
+                  justifyContent: 'center',
+                  alignItems: 'center',
+                  fontSize: '2rem',
+                }}
+              >
+                £{formatNumberWithCommas(monthDifference)}
+              </Typography>
+              {/* {differenceIcon} */}
+            </Box>
+          ) : null}
+        </Box>
+
+        {/* <SnapshotDetailOverviewTotal snapshot={snapshot} /> */}
+      </Box>
     </Paper>
   );
 };

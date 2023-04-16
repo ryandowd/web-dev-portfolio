00d660a4fc50f5446964341b268cc819e3681201
ryandowd <ryan.dowd84@gmail.com>
2023-04-16 14:51:42 +0800

    Debug finance page

--- src/sites/finance/components/SnapshotTotalsList.tsx
+++ src/sites/finance/components/SnapshotTotalsList.tsx
@@ -1,4 +1,5 @@
 import { Snapshot } from '@/sites/finance/global/types';
+import { Box } from '@mui/system';
 
 type SnapshotTotalsListProps = {
   snapshots: Snapshot[];
@@ -6,5 +7,30 @@ type SnapshotTotalsListProps = {
 
 export const SnapshotTotalsList = (props: SnapshotTotalsListProps) => {
   console.log('props', props);
-  return <h1>Hello</h1>;
+  const { snapshots } = props;
+  return (
+    <>
+      {snapshots?.map((snapshot) => {
+        return (
+          <Box
+            key={snapshot.snapshotId}
+            sx={{
+              marginBottom: '40px',
+              '.MuiPaper-root': {
+                transition: 'all 0.3s ease-in-out',
+              },
+              '&:hover .MuiPaper-root': {
+                transform: 'scale(1.03)',
+                cursor: 'pointer',
+              },
+            }}
+          >
+            {/* <Link href={`/finance-tracker/snapshots/${snapshot.snapshotId}`}>
+              <SnapshotDetailOverview snapshot={snapshot} />
+            </Link> */}
+          </Box>
+        );
+      })}
+    </>
+  );
 };

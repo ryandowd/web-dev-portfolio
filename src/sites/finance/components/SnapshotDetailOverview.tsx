8388a389464b85f6bd5d28a001dfa03433d17b62
ryandowd <ryan.dowd84@gmail.com>
2023-04-16 15:36:07 +0800

    Debug finance page

--- src/sites/finance/components/SnapshotDetailOverview.tsx
+++ src/sites/finance/components/SnapshotDetailOverview.tsx
@@ -20,20 +20,19 @@ export const SnapshotDetailOverview = (props: SnapshotDetailOverviewProps) => {
   // @TODO - Fix this
   const isDifferenceNegative = (monthDifference || 0) < 0;
 
-  console.log('isDifferenceNegative', isDifferenceNegative);
   const differenceColour = isDifferenceNegative ? 'red' : 'green';
-  const differenceIcon = isDifferenceNegative ? (
-    <ArrowDropDown sx={{ color: differenceColour }} />
-  ) : (
-    <ArrowDropUp sx={{ color: differenceColour }} />
-  );
+  // const differenceIcon = isDifferenceNegative ? (
+  //   <ArrowDropDown sx={{ color: differenceColour }} />
+  // ) : (
+  //   <ArrowDropUp sx={{ color: differenceColour }} />
+  // );
 
-  const transparentStyles = isTransparent
-    ? {
-        backgroundColor: 'transparent',
-        boxShadow: 'none',
-      }
-    : {};
+  // const transparentStyles = isTransparent
+  //   ? {
+  //       backgroundColor: 'transparent',
+  //       boxShadow: 'none',
+  //     }
+  //   : {};
 
   return (
     <Paper

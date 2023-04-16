96b13f8726cfc0570bd6cd57fcb79605152f7abb
ryandowd <ryan.dowd84@gmail.com>
2023-04-16 14:47:01 +0800

    Debug finance page

--- src/sites/finance/components/SnapshotTotalsList.tsx
+++ src/sites/finance/components/SnapshotTotalsList.tsx
@@ -1,6 +1,3 @@
-// import { Box } from '@mui/material';
-// import Link from 'next/link';
-// import { SnapshotDetailOverview } from './SnapshotDetailOverview';
 import { Snapshot } from '@/sites/finance/global/types';
 
 type SnapshotTotalsListProps = {
@@ -8,35 +5,6 @@ type SnapshotTotalsListProps = {
 };
 
 export const SnapshotTotalsList = (props: SnapshotTotalsListProps) => {
-  const { snapshots } = props;
-
-  console.log('snapshots', snapshots);
-
+  console.log('props', props);
   return <h1>Hello</h1>;
-
-  // return (
-  //   <>
-  //     {snapshots?.map((snapshot) => {
-  //       return (
-  //         <Box
-  //           key={snapshot.snapshotId}
-  //           sx={{
-  //             marginBottom: '40px',
-  //             '.MuiPaper-root': {
-  //               transition: 'all 0.3s ease-in-out',
-  //             },
-  //             '&:hover .MuiPaper-root': {
-  //               transform: 'scale(1.03)',
-  //               cursor: 'pointer',
-  //             },
-  //           }}
-  //         >
-  //           <Link href={`/finance-tracker/snapshots/${snapshot.snapshotId}`}>
-  //             <SnapshotDetailOverview snapshot={snapshot} />
-  //           </Link>
-  //         </Box>
-  //       );
-  //     })}
-  //   </>
-  // );
 };
